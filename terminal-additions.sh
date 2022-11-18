b77e55ebb2fea057a18d839ce2b8702cbbdf5ab4
diego pastor <dieg0407@hotmail.com>
2022-11-17 20:44:51 -0500

    internal additions to bash

---
+++ terminal-additions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install bat neofetch cpufetch bpytop -y
+sudo apt install fzf bc -y
+sudo npm install -g tldr
+
+echo -e "Install synth-shell: (https://github.com/andresgongora/synth-shell)"
